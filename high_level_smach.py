f1278435494858ece92cb012264978d5c6d8dd4e
tbohne <tim.bohne@dfki.de>
2022-06-30 14:47:23 +0200

    refine high-level SMACH

--- high_level_smach.py
+++ high_level_smach.py
@@ -3,7 +3,8 @@
 # @author Tim Bohne
 
 import smach
-import time
+import subprocess
+from bs4 import BeautifulSoup
 
 
 class RecCustomerComplaints(smach.State):
@@ -11,36 +12,21 @@ class RecCustomerComplaints(smach.State):
     def __init__(self):
 
         smach.State.__init__(self,
-                             outcomes=['waiting_for_complaints', 'complaints_received'],
+                             outcomes=['waiting_for_complaints', 'complaints_received', 'no_complaints'],
                              input_keys=['input_info'],
-                             output_keys=['proc_customer_complaints'])
+                             output_keys=['interview_protocol_file'])
 
-    def get_complaints(self):
-        return ""
+    @staticmethod
+    def launch_customer_xps():
+        print("launching customer XPS..")
+        subprocess.call(['java', '-jar', 'CustomerXPS.jar'])
 
     def execute(self, userdata):
         print("executing REC_CUSTOMER_COMPLAINTS state..")
-
-        if len(userdata.input_info) > 0:
-            print("input_info: %s", userdata.input_info)
-            userdata.proc_customer_complaints = userdata.input_info
-            return "complaints_received"
-
-        print("waiting for customer complaints..")
-        complaints = self.get_complaints()
-
-        if complaints:
-            print("received complaints..")
-            if len(complaints) == 0:
-                print("empty complaints..")
-                return "waiting_for_complaints"
-
-            userdata.proc_customer_complaints = complaints
-            return "complaints_received"
-        else:
-            print("waiting for complaints..")
-            time.sleep(10)
-            return "waiting_for_complaints"
+        self.launch_customer_xps()
+        print("customer XPS session protocol saved..")
+        userdata.interview_protocol_file = "./KB/session_res.xml"
+        return "complaints_received"
 
 
 class EstablishContextOrHypothesis(smach.State):
@@ -48,14 +34,37 @@ class EstablishContextOrHypothesis(smach.State):
     def __init__(self):
         smach.State.__init__(self,
                              outcomes=['established_context', 'established_hypothesis', 'unknown'],
-                             input_keys=['proc_customer_complaints'],
+                             input_keys=['interview_protocol_file'],
                              output_keys=['context', 'hypothesis'])
 
     def execute(self, userdata):
         print("executing ESTABLISH_CONTEXT_OR_HYPOTHESIS state..")
+        print("reading XML protocol..")
 
-        userdata.hypothesis = "failure X"
-        return 'hypothesis'
+        with open(userdata.interview_protocol_file) as f:
+            data = f.read()
+        session_data = BeautifulSoup(data, 'xml')
+
+        # print(session_data.prettify())
+
+        for tag in session_data.find_all('rating', {'type': 'heuristic'}):
+            res = tag.parent['objectName']
+
+        userdata.hypothesis = res
+        return 'established_hypothesis'
+
+
+class ReadOBDInformation(smach.State):
+
+    def __init__(self):
+        smach.State.__init__(self,
+                             outcomes=['read_OBD_info'],
+                             input_keys=['hypothesis', 'context'],
+                             output_keys=['measuring_points'])
+
+    def execute(self, userdata):
+        print("executing READ_OBD_INFORMATION state..")
+        return 'read_OBD_info'
 
 
 class VehicleDiagnosisAndRecommendationStateMachine(smach.StateMachine):
@@ -71,15 +80,21 @@ class VehicleDiagnosisAndRecommendationStateMachine(smach.StateMachine):
         with self:
             self.add('REC_CUSTOMER_COMPLAINTS', RecCustomerComplaints(),
                      transitions={'waiting_for_complaints': 'REC_CUSTOMER_COMPLAINTS',
-                                  'complaints_received': 'ESTABLISH_CONTEXT_OR_HYPOTHESIS'},
+                                  'complaints_received': 'ESTABLISH_CONTEXT_OR_HYPOTHESIS',
+                                  'no_complaints': 'READ_OBD_INFORMATION'},
                      remapping={'input_info': 'sm_input',
-                                'proc_customer_complaints': 'sm_input'})
+                                'interview_protocol_file': 'sm_input'})
 
             self.add('ESTABLISH_CONTEXT_OR_HYPOTHESIS', EstablishContextOrHypothesis(),
                      transitions={'established_context': 'no_diagnosis',
                                   'established_hypothesis': 'diagnosis_without_recommendation',
                                   'unknown': 'no_diagnosis'},
-                     remapping={'hypothesis': 'sm_input'})
+                     remapping={'interview_protocol_file': 'sm_input',
+                                'hypothesis': 'sm_input'})
+
+            self.add('READ_OBD_INFORMATION', ReadOBDInformation(),
+                     transitions={'read_OBD_info': 'no_diagnosis'},
+                     remapping={})
 
 
 if __name__ == '__main__':
