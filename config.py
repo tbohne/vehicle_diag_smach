446e391917e553cd5057069815c6cd9429274731
tbohne <tim.bohne@dfki.de>
2022-09-20 16:46:58 +0200

    fix config

--- config.py
+++ config.py
@@ -4,7 +4,7 @@
 
 CUSTOMER_XPS = "res/CustomerXPS.jar"
 INTERVIEW_PROTOCOL_FILE = "res/session_res.xml"
-OBD_ONTOLOGY_PATH = "../OBDOntology"
-ONTOLOGY_FILE = "obd_ontology.owl"
+OBD_ONTOLOGY_PATH = "OBDOntology/"
+ONTOLOGY_FILE = "static_obd_ontology.owl"
 TRAINED_MODEL = "res/best_model.h5"
 DUMMY_OSCILLOSCOPE = "res/dummy_oscilloscope.csv"
