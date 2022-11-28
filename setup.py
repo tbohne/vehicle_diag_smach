95eb772180c2e3238b5600bf8f7d4cd2300bb775
tbohne <tim.bohne@dfki.de>
2022-11-28 17:00:30 +0100

    add setup / requirements

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+__version__ = '0.0.1'
+URL = 'https://github.com/tbohne/vehicle_diag_smach'
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='vehicle_diag_smach',
+    version=__version__,
+    description='State-machine-based prototype of a vehicle diagnosis system.',
+    author='Tim Bohne',
+    author_email='tim.bohne@dfki.de',
+    url=URL,
+    download_url=f'{URL}/archive/{__version__}.tar.gz',
+    keywords=[
+        'neural-network',
+        'state-machine',
+        'knowledge-graph',
+        'automotive',
+        'anomaly-detection'
+    ],
+    python_requires='>=3.7',
+    install_requires=required,
+    packages=find_packages(),
+    include_package_data=True,
+)
