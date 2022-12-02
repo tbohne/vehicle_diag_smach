63a4565e2eb017c94c7eff0f93f7d4e2709e0192
tbohne <tim.bohne@dfki.de>
2022-12-02 18:10:30 +0100

    launch fuseki server from binary as daemon

--- docker/run_services.sh
+++ docker/run_services.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/usr/bin/java -Xms4g -Xmx8g -Dlog4j2.formatMsgNoLookups=true -XX:-UseGCOverheadLimit -XX:+UseParallelGC -jar fuseki-server.jar --loc=/code/fuseki &
+./apache-jena-fuseki-4.6.1/fuseki-server &
 python3 /code/vehicle_diag_smach/high_level_smach.py
