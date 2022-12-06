54a217b8d58eddf2b2f9f8c4a9400c609be54aca
tbohne <tim.bohne@dfki.de>
2022-12-06 16:44:31 +0100

    create side-by-side tmux session for fuseki server and smach in docker container

--- docker/run_services.sh
+++ docker/run_services.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-./apache-jena-fuseki-4.6.1/fuseki-server &
-python3 /code/vehicle_diag_smach/high_level_smach.py
+# create a new session "session_one"
+tmux new-session -d -s session_one
+
+# run fuseki server
+tmux send-keys -t session_one "./apache-jena-fuseki-4.6.1/fuseki-server" Enter
+
+# split terminal window horizontally
+tmux split-window -h -t session_one
+
+# run smach in the second window
+tmux send-keys -t session_one "python3 /code/vehicle_diag_smach/high_level_smach.py" Enter
+
+# attach to session
+tmux attach -t session_one
