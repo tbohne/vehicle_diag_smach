fdc3e6ae2e4347ba7806fb13d3531e7a2c1e0ae7
tbohne <tim.bohne@dfki.de>
2023-04-28 15:01:25 +0200

    implement data provider interface

---
+++ vehicle_diag_smach/data_provider.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @author Tim Bohne
+
+from abc import ABC, abstractmethod
+
+from vehicle_diag_smach.data_types.intermediate_results import IntermediateResults
+
+
+class DataProvider(ABC):
+    """
+    Interface that defines the state machine's provision of intermediate results to be displayed in the hub UI.
+    """
+
+    @abstractmethod
+    def provide_intermediate_results(self) -> IntermediateResults:
+        pass
