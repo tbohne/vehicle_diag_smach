345caef2a95a2d9c5b305d267cd86c4c29712803
tbohne <tim.bohne@dfki.de>
2023-04-28 14:22:22 +0200

    impl intermediate results type

---
+++ vehicle_diag_smach/data_types/intermediate_results.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @author Tim Bohne
+
+class IntermediateResults:
+    """
+    Represents intermediate results of the diagnosis, which are communicated to the user.
+    """
+
+    def __init__(self, status: str, required_actions: list[dict], state_log: list):
+        """
+        Inits the intermediate results.
+
+        :param status: status of the diagnostic process
+        :param required_actions: actions required from the user (e.g. record data at component)
+        :param state_log: history of past states
+        """
+        self.status = status
+        self.required_actions = required_actions
+        self.state_log = state_log
