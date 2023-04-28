e5a06474e35e23a2b59a12bc08f39919bcb7fdba
tbohne <tim.bohne@dfki.de>
2023-04-28 14:11:00 +0200

    implement interface for diag data access

---
+++ vehicle_diag_smach/data_accessor.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @author Tim Bohne
+
+from abc import ABC, abstractmethod
+
+from vehicle_diag_smach.onboard_diagnosis_data import OnboardDiagnosisData
+from vehicle_diag_smach.oscillogram_data import OscillogramData
+from vehicle_diag_smach.workshop_data import WorkshopData
+
+
+class DataAccessor(ABC):
+    """
+    Interface that defines the state machine's access to measurements and diagnosis-relevant case data.
+    """
+
+    @abstractmethod
+    def get_workshop_info(self) -> WorkshopData:
+        pass
+
+    @abstractmethod
+    def get_obd_data(self) -> OnboardDiagnosisData:
+        pass
+
+    @abstractmethod
+    def get_oscillogram_by_component(self, component: str) -> OscillogramData:
+        pass
