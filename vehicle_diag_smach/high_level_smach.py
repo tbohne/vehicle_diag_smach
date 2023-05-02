8b30914d369f49883e0196869f58d148e05abf26
tbohne <tim.bohne@dfki.de>
2023-05-02 15:35:55 +0200

    implement / refine ESTABLISH_INITIAL_HYPOTHESIS state

--- vehicle_diag_smach/high_level_smach.py
+++ vehicle_diag_smach/high_level_smach.py
@@ -2,84 +2,20 @@
 # -*- coding: utf-8 -*-
 # @author Tim Bohne
 
-import json
 import logging
 import os
 
 import smach
 import tensorflow as tf
-from bs4 import BeautifulSoup
 from obd_ontology import knowledge_graph_query_tool
 from termcolor import colored
 
-from config import SESSION_DIR, XPS_SESSION_FILE, HISTORICAL_INFO_FILE, CC_TMP_FILE
+from config import SESSION_DIR, HISTORICAL_INFO_FILE
 from vehicle_diag_smach.diagnosis import DiagnosisStateMachine
 from high_level_states.rec_vehicle_and_proc_metadata import RecVehicleAndProcMetadata
 from high_level_states.proc_customer_complaints import ProcCustomerComplaints
 from high_level_states.read_obd_data_and_gen_ontology_instances import ReadOBDDataAndGenOntologyInstances
-
-
-class EstablishInitialHypothesis(smach.State):
-    """
-    State in the high-level SMACH that represents situations in which an initial hypothesis is established based
-    on the provided information.
-    """
-
-    def __init__(self):
-        smach.State.__init__(self,
-                             outcomes=['established_init_hypothesis', 'no_OBD_and_no_CC'],
-                             input_keys=['vehicle_specific_instance_data'],
-                             output_keys=['hypothesis'])
-
-    def execute(self, userdata: smach.user_data.Remapper) -> str:
-        """
-        Execution of 'ESTABLISH_INITIAL_HYPOTHESIS' state.
-
-        :param userdata: input of state
-        :return: outcome of the state ("established_init_hypothesis" | "no_OBD_and_no_CC")
-        """
-        os.system('cls' if os.name == 'nt' else 'clear')
-        print("\n\n############################################")
-        print("executing", colored("ESTABLISH_INITIAL_HYPOTHESIS", "yellow", "on_grey", ["bold"]), "state..")
-        print("############################################")
-
-        print("\nreading customer complaints session protocol..")
-        initial_hypothesis = ""
-        try:
-            with open(SESSION_DIR + "/" + XPS_SESSION_FILE) as f:
-                data = f.read()
-                session_data = BeautifulSoup(data, 'xml')
-                for tag in session_data.find_all('rating', {'type': 'heuristic'}):
-                    initial_hypothesis = tag.parent['objectName']
-        except FileNotFoundError:
-            print("no customer complaints available..")
-
-        if len(userdata.vehicle_specific_instance_data['dtc_list']) == 0 and len(initial_hypothesis) == 0:
-            # no OBD data + no customer complaints -> insufficient data
-            return "no_OBD_and_no_CC"
-
-        print("reading historical information..")
-        with open(SESSION_DIR + "/" + HISTORICAL_INFO_FILE) as f:
-            data = f.read()
-
-        if len(initial_hypothesis) > 0:
-            print("initial hypothesis based on customer complaints available..")
-            print("initial hypothesis:", initial_hypothesis)
-            userdata.hypothesis = initial_hypothesis
-            unused_cc = {'list': [initial_hypothesis]}
-            with open(SESSION_DIR + "/" + CC_TMP_FILE, 'w') as f:
-                json.dump(unused_cc, f, default=str)
-        else:
-            print("no initial hypothesis based on customer complaints..")
-
-        # TODO: use historical data to refine initial hypothesis (e.g. to deny certain hypotheses)
-        print("establish hypothesis..")
-
-        val = None
-        while val != "":
-            val = input("\n..............................")
-
-        return "established_init_hypothesis"
+from high_level_states.establish_initial_hypothesis import EstablishInitialHypothesis
 
 
 class RetrieveHistoricalData(smach.State):
---
+++ vehicle_diag_smach/high_level_states/establish_initial_hypothesis.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @author Tim Bohne
+
+import json
+import os
+
+import smach
+from bs4 import BeautifulSoup
+from termcolor import colored
+
+from vehicle_diag_smach.config import SESSION_DIR, XPS_SESSION_FILE, HISTORICAL_INFO_FILE, CC_TMP_FILE
+
+
+class EstablishInitialHypothesis(smach.State):
+    """
+    State in the high-level SMACH that represents situations in which an initial hypothesis is established based
+    on the provided information.
+    """
+
+    def __init__(self):
+        smach.State.__init__(self,
+                             outcomes=['established_init_hypothesis', 'no_OBD_and_no_CC'],
+                             input_keys=['vehicle_specific_instance_data'],
+                             output_keys=['hypothesis'])
+
+    def execute(self, userdata: smach.user_data.Remapper) -> str:
+        """
+        Execution of 'ESTABLISH_INITIAL_HYPOTHESIS' state.
+
+        :param userdata: input of state
+        :return: outcome of the state ("established_init_hypothesis" | "no_OBD_and_no_CC")
+        """
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("\n\n############################################")
+        print("executing", colored("ESTABLISH_INITIAL_HYPOTHESIS", "yellow", "on_grey", ["bold"]), "state..")
+        print("############################################")
+
+        print("\nreading customer complaints session protocol..")
+        initial_hypothesis = ""
+        try:
+            with open(SESSION_DIR + "/" + XPS_SESSION_FILE) as f:
+                data = f.read()
+                session_data = BeautifulSoup(data, 'xml')
+                for tag in session_data.find_all('rating', {'type': 'heuristic'}):
+                    initial_hypothesis = tag.parent['objectName']
+        except FileNotFoundError:
+            print("no customer complaints available..")
+
+        if len(userdata.vehicle_specific_instance_data['dtc_list']) == 0 and len(initial_hypothesis) == 0:
+            # no OBD data + no customer complaints -> insufficient data
+            return "no_OBD_and_no_CC"
+
+        print("reading historical information..")
+        with open(SESSION_DIR + "/" + HISTORICAL_INFO_FILE) as f:
+            data = f.read()
+
+        if len(initial_hypothesis) > 0:
+            print("initial hypothesis based on customer complaints available..")
+            print("initial hypothesis:", initial_hypothesis)
+            userdata.hypothesis = initial_hypothesis
+            unused_cc = {'list': [initial_hypothesis]}
+            with open(SESSION_DIR + "/" + CC_TMP_FILE, 'w') as f:
+                json.dump(unused_cc, f, default=str)
+        else:
+            print("no initial hypothesis based on customer complaints..")
+
+        # TODO: use historical data to refine initial hypothesis (e.g. to deny certain hypotheses)
+        print("establish hypothesis..")
+
+        val = None
+        while val != "":
+            val = input("\n..............................")
+
+        return "established_init_hypothesis"
