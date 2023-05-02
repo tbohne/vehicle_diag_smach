7c55abd5e3a584a940bcab01c4e15705edd5bb64
tbohne <tim.bohne@dfki.de>
2023-05-02 15:41:48 +0200

    implement / refine RETRIEVE_HISTORICAL_DATA state

--- vehicle_diag_smach/high_level_smach.py
+++ vehicle_diag_smach/high_level_smach.py
@@ -3,71 +3,17 @@
 # @author Tim Bohne
 
 import logging
-import os
 
 import smach
 import tensorflow as tf
-from obd_ontology import knowledge_graph_query_tool
-from termcolor import colored
 
-from config import SESSION_DIR, HISTORICAL_INFO_FILE
 from vehicle_diag_smach.diagnosis import DiagnosisStateMachine
 from high_level_states.rec_vehicle_and_proc_metadata import RecVehicleAndProcMetadata
 from high_level_states.proc_customer_complaints import ProcCustomerComplaints
 from high_level_states.read_obd_data_and_gen_ontology_instances import ReadOBDDataAndGenOntologyInstances
 from high_level_states.establish_initial_hypothesis import EstablishInitialHypothesis
+from high_level_states.retrieve_historical_data import RetrieveHistoricalData
 
-
-class RetrieveHistoricalData(smach.State):
-    """
-    State in the high-level SMACH that represents situations in which historical information are retrieved for the
-    given car (individually, not type), i.e., information that we accumulated in previous repair sessions.
-    Optionally, historical data for the car model can be retrieved.
-    """
-
-    def __init__(self):
-        smach.State.__init__(self,
-                             outcomes=['processed_all_data'],
-                             input_keys=['vehicle_specific_instance_data_in'],
-                             output_keys=['vehicle_specific_instance_data_out'])
-
-    def execute(self, userdata: smach.user_data.Remapper) -> str:
-        """
-        Execution of 'RETRIEVE_HISTORICAL_DATA' state.
-
-        Two kinds of information:
-        - historical info for specific vehicle (via VIN)
-        - historical info for vehicle type (via model)
-
-        :param userdata: input of state
-        :return: outcome of the state ("processed_all_data")
-        """
-        os.system('cls' if os.name == 'nt' else 'clear')
-        print("\n\n############################################")
-        print("executing", colored("RETRIEVE_HISTORICAL_DATA", "yellow", "on_grey", ["bold"]), "state..")
-        print("############################################\n")
-        qt = knowledge_graph_query_tool.KnowledgeGraphQueryTool(local_kb=False)
-        vin = userdata.vehicle_specific_instance_data_in['vin']
-        model = userdata.vehicle_specific_instance_data_in['model']
-
-        # TODO: potentially retrieve more historical information (not only DTCs)
-        historic_dtcs_by_vin = qt.query_dtcs_by_vin(vin)
-        print("DTCs previously recorded in present car:", historic_dtcs_by_vin)
-        print("\nmodel to retrieve historical data for:", model, "\n")
-        historic_dtcs_by_model = qt.query_dtcs_by_model(model)
-        print("DTCs previously recorded in model of present car:", historic_dtcs_by_model)
-
-        with open(SESSION_DIR + "/" + HISTORICAL_INFO_FILE, "w") as f:
-            f.write("DTCs previously recorded in car with VIN " + vin + ": " + str(historic_dtcs_by_vin) + "\n")
-            f.write("DTCs previously recorded in cars of model " + model + ": " + str(historic_dtcs_by_model) + "\n")
-
-        userdata.vehicle_specific_instance_data_out = userdata.vehicle_specific_instance_data_in
-
-        val = None
-        while val != "":
-            val = input("\n..............................")
-
-        return "processed_all_data"
 
 class VehicleDiagnosisStateMachine(smach.StateMachine):
     """
---
+++ vehicle_diag_smach/high_level_states/retrieve_historical_data.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @author Tim Bohne
+
+import os
+
+import smach
+from obd_ontology import knowledge_graph_query_tool
+from termcolor import colored
+
+from vehicle_diag_smach.config import SESSION_DIR, HISTORICAL_INFO_FILE
+
+
+class RetrieveHistoricalData(smach.State):
+    """
+    State in the high-level SMACH that represents situations in which historical information are retrieved for the
+    given car (individually, not type), i.e., information that we accumulated in previous repair sessions.
+    Optionally, historical data for the car model can be retrieved.
+    """
+
+    def __init__(self):
+        smach.State.__init__(self,
+                             outcomes=['processed_all_data'],
+                             input_keys=['vehicle_specific_instance_data_in'],
+                             output_keys=['vehicle_specific_instance_data_out'])
+
+    def execute(self, userdata: smach.user_data.Remapper) -> str:
+        """
+        Execution of 'RETRIEVE_HISTORICAL_DATA' state.
+
+        Two kinds of information:
+        - historical info for specific vehicle (via VIN)
+        - historical info for vehicle type (via model)
+
+        :param userdata: input of state
+        :return: outcome of the state ("processed_all_data")
+        """
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("\n\n############################################")
+        print("executing", colored("RETRIEVE_HISTORICAL_DATA", "yellow", "on_grey", ["bold"]), "state..")
+        print("############################################\n")
+        qt = knowledge_graph_query_tool.KnowledgeGraphQueryTool(local_kb=False)
+        vin = userdata.vehicle_specific_instance_data_in['vin']
+        model = userdata.vehicle_specific_instance_data_in['model']
+
+        # TODO: potentially retrieve more historical information (not only DTCs)
+        historic_dtcs_by_vin = qt.query_dtcs_by_vin(vin)
+        print("DTCs previously recorded in present car:", historic_dtcs_by_vin)
+        print("\nmodel to retrieve historical data for:", model, "\n")
+        historic_dtcs_by_model = qt.query_dtcs_by_model(model)
+        print("DTCs previously recorded in model of present car:", historic_dtcs_by_model)
+
+        with open(SESSION_DIR + "/" + HISTORICAL_INFO_FILE, "w") as f:
+            f.write("DTCs previously recorded in car with VIN " + vin + ": " + str(historic_dtcs_by_vin) + "\n")
+            f.write("DTCs previously recorded in cars of model " + model + ": " + str(historic_dtcs_by_model) + "\n")
+
+        userdata.vehicle_specific_instance_data_out = userdata.vehicle_specific_instance_data_in
+
+        val = None
+        while val != "":
+            val = input("\n..............................")
+
+        return "processed_all_data"
