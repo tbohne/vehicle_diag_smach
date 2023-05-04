998b397bfff8d0fcf1e15e18ded3a5e9a1b4aee6
tbohne <tim.bohne@dfki.de>
2023-05-04 13:09:48 +0200

    apply data accessor interface in READ_OBD_DATA

--- vehicle_diag_smach/high_level_states/read_obd_data_and_gen_ontology_instances.py
+++ vehicle_diag_smach/high_level_states/read_obd_data_and_gen_ontology_instances.py
@@ -9,7 +9,8 @@ import smach
 from obd_ontology import ontology_instance_generator
 from termcolor import colored
 
-from vehicle_diag_smach.config import SAMPLE_OBD_LOG, SESSION_DIR, OBD_INFO_FILE, DTC_TMP_FILE, OBD_ONTOLOGY_PATH
+from vehicle_diag_smach.config import SESSION_DIR, OBD_INFO_FILE, DTC_TMP_FILE, OBD_ONTOLOGY_PATH
+from vehicle_diag_smach.interfaces.data_accessor import DataAccessor
 
 
 class ReadOBDDataAndGenOntologyInstances(smach.State):
@@ -19,33 +20,14 @@ class ReadOBDDataAndGenOntologyInstances(smach.State):
     is entered into the knowledge graph.
     """
 
-    def __init__(self):
+    def __init__(self, data_accessor: DataAccessor):
 
         smach.State.__init__(self,
                              outcomes=['processed_OBD_data', 'no_OBD_data'],
                              input_keys=[''],
                              output_keys=['vehicle_specific_instance_data'])
 
-    @staticmethod
-    def parse_obd_logfile() -> dict:
-        print("\nprocessing OBD log file..")
-        obd_data = {"dtc_list": [], "model": "", "hsn": "", "tsn": "", "vin": ""}
-
-        with open(SAMPLE_OBD_LOG) as f:
-            obd_lines = f.readlines()
-
-        # TODO: parse DTCs from OBD log (above)
-        obd_data['dtc_list'] = ['P2563']
-        obd_data['model'] = "Mazda 3"
-        obd_data['hsn'] = "849357984"
-        obd_data['tsn'] = "453948539"
-        obd_data['vin'] = "1234567890ABCDEFGHJKLMNPRSTUVWXYZ"
-
-        for k in obd_data.keys():
-            print(k + ": " + str(obd_data[k]))
-        print()
-
-        return obd_data
+        self.data_accessor = data_accessor
 
     def execute(self, userdata: smach.user_data.Remapper) -> str:
         """
@@ -59,29 +41,26 @@ class ReadOBDDataAndGenOntologyInstances(smach.State):
         print("executing", colored("READ_OBD_DATA_AND_GEN_ONTOLOGY_INSTANCES", "yellow", "on_grey", ["bold"]),
               "state..")
         print("############################################")
-        vehicle_specific_instance_data = self.parse_obd_logfile()
-        if len(vehicle_specific_instance_data['dtc_list']) == 0:
+        obd_data = self.data_accessor.get_obd_data()
+        if len(obd_data.dtc_list) == 0:
             return "no_OBD_data"
 
         # write OBD data to session file
         with open(SESSION_DIR + "/" + OBD_INFO_FILE, "w") as f:
-            json.dump(vehicle_specific_instance_data, f, default=str)
+            json.dump(obd_data, f, default=str)
         # also create tmp file for unused DTC instances
         with open(SESSION_DIR + "/" + DTC_TMP_FILE, "w") as f:
-            dtc_tmp = {'list': vehicle_specific_instance_data['dtc_list']}
+            dtc_tmp = {'list': obd_data.dtc_list}
             json.dump(dtc_tmp, f, default=str)
 
         # extend knowledge graph with read OBD data (if the vehicle instance already exists, it will be extended)
         instance_gen = ontology_instance_generator.OntologyInstanceGenerator(OBD_ONTOLOGY_PATH, local_kb=False)
-        for dtc in vehicle_specific_instance_data['dtc_list']:
-            instance_gen.extend_knowledge_graph(
-                vehicle_specific_instance_data['model'], vehicle_specific_instance_data['hsn'],
-                vehicle_specific_instance_data['tsn'], vehicle_specific_instance_data['vin'], dtc
-            )
+        for dtc in obd_data.dtc_list:
+            instance_gen.extend_knowledge_graph(obd_data.model, obd_data.hsn, obd_data.tsn, obd_data.vin, dtc)
 
         val = None
         while val != "":
             val = input("\n..............................")
 
-        userdata.vehicle_specific_instance_data = vehicle_specific_instance_data
+        userdata.vehicle_specific_instance_data = obd_data
         return "processed_OBD_data"
