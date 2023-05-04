c2b6b36b1134ade985fc6aac5d715cc12d921b15
tbohne <tim.bohne@dfki.de>
2023-05-04 13:05:39 +0200

    impl data accessor in high-level smach

--- vehicle_diag_smach/high_level_smach.py
+++ vehicle_diag_smach/high_level_smach.py
@@ -7,12 +7,15 @@ import logging
 import smach
 import tensorflow as tf
 
-from high_level_states.establish_initial_hypothesis import EstablishInitialHypothesis
-from high_level_states.proc_customer_complaints import ProcCustomerComplaints
-from high_level_states.read_obd_data_and_gen_ontology_instances import ReadOBDDataAndGenOntologyInstances
-from high_level_states.rec_vehicle_and_proc_metadata import RecVehicleAndProcMetadata
-from high_level_states.retrieve_historical_data import RetrieveHistoricalData
 from vehicle_diag_smach.diagnosis import DiagnosisStateMachine
+from vehicle_diag_smach.high_level_states.establish_initial_hypothesis import EstablishInitialHypothesis
+from vehicle_diag_smach.high_level_states.proc_customer_complaints import ProcCustomerComplaints
+from vehicle_diag_smach.high_level_states.read_obd_data_and_gen_ontology_instances import \
+    ReadOBDDataAndGenOntologyInstances
+from vehicle_diag_smach.high_level_states.rec_vehicle_and_proc_metadata import RecVehicleAndProcMetadata
+from vehicle_diag_smach.high_level_states.retrieve_historical_data import RetrieveHistoricalData
+from vehicle_diag_smach.interfaces.data_accessor import DataAccessor
+from vehicle_diag_smach.io.local_data_accessor import LocalDataAccessor
 
 
 class VehicleDiagnosisStateMachine(smach.StateMachine):
@@ -20,12 +23,13 @@ class VehicleDiagnosisStateMachine(smach.StateMachine):
     High-level hierarchically structured state machine guiding the entire vehicle diagnosis process.
     """
 
-    def __init__(self):
+    def __init__(self, data_accessor: DataAccessor):
         super(VehicleDiagnosisStateMachine, self).__init__(
             outcomes=['diag', 'insufficient_data', 'refuted_hypothesis'],
             input_keys=[],
             output_keys=[]
         )
+        self.data_accessor = data_accessor
         self.userdata.sm_input = []
 
         with self:
@@ -40,7 +44,7 @@ class VehicleDiagnosisStateMachine(smach.StateMachine):
                      remapping={'user_data': 'sm_input',
                                 'interview_protocol_file': 'sm_input'})
 
-            self.add('READ_OBD_DATA_AND_GEN_ONTOLOGY_INSTANCES', ReadOBDDataAndGenOntologyInstances(),
+            self.add('READ_OBD_DATA_AND_GEN_ONTOLOGY_INSTANCES', ReadOBDDataAndGenOntologyInstances(self.data_accessor),
                      transitions={'processed_OBD_data': 'RETRIEVE_HISTORICAL_DATA',
                                   'no_OBD_data': 'ESTABLISH_INITIAL_HYPOTHESIS'},
                      remapping={'interview_data': 'sm_input',
@@ -78,17 +82,13 @@ def log_err(msg):
     print("[ ERROR ] : " + str(msg))
 
 
-def run():
-    """
-    Runs the state machine.
-    """
+if __name__ == '__main__':
     # set custom logging functions
     smach.set_loggers(log_info, log_debug, log_warn, log_err)
 
-    sm = VehicleDiagnosisStateMachine()
+    # init local implementations of I/O interfaces
+    data_acc = LocalDataAccessor()
+
+    sm = VehicleDiagnosisStateMachine(data_acc)
     tf.get_logger().setLevel(logging.ERROR)
     sm.execute()
-
-
-if __name__ == '__main__':
-    run()
