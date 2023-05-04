ac7c52a5fb65912a08d1aeba81aec53790930750
tbohne <tim.bohne@dfki.de>
2023-05-04 13:11:36 +0200

    impl data accessor interface for local usage

---
+++ vehicle_diag_smach/io/local_data_accessor.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @author Tim Bohne
+
+from vehicle_diag_smach.data_types.onboard_diagnosis_data import OnboardDiagnosisData
+from vehicle_diag_smach.data_types.oscillogram_data import OscillogramData
+from vehicle_diag_smach.data_types.workshop_data import WorkshopData
+from vehicle_diag_smach.interfaces.data_accessor import DataAccessor
+
+
+class LocalDataAccessor(DataAccessor):
+    """
+    Implementation of the `DataAccessor` interface using local files and dummy data for I/O.
+    """
+
+    def __init__(self):
+        pass
+
+    def get_workshop_info(self) -> WorkshopData:
+        pass
+
+    def get_obd_data(self) -> OnboardDiagnosisData:
+        print("\nprocessing OBD data..")
+        obd_data = OnboardDiagnosisData(
+            ['P2563'], "Mazda 3", "849357984", "453948539", "1234567890ABCDEFGHJKLMNPRSTUVWXYZ"
+        )
+        print(obd_data)
+        return obd_data
+
+    def get_oscillogram_by_component(self, component: str) -> OscillogramData:
+        pass
