95b98f0d4e466c08390b124935985f1d4741fa4f
tbohne <tim.bohne@dfki.de>
2023-05-05 09:54:32 +0200

    extend osci data type

--- vehicle_diag_smach/data_types/oscillogram_data.py
+++ vehicle_diag_smach/data_types/oscillogram_data.py
@@ -10,10 +10,12 @@ class OscillogramData:
     Represents oscillogram data, which is communicated to the state machine.
     """
 
-    def __init__(self, time_series: List[float]):
+    def __init__(self, time_series: List[float], comp_name: str):
         """
         Inits the oscillogram data.
 
         :param time_series: recorded signal (voltage values over time)
+        :param comp_name: name of the component the signal belongs to
         """
         self.time_series = time_series
+        self.comp_name = comp_name
