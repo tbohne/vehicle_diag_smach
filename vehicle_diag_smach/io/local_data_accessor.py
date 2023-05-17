5ec4bc039d30869e775827aa850998b64ce625d7
tbohne <tim.bohne@dfki.de>
2023-05-17 16:30:16 +0200

    implement local I/O

--- vehicle_diag_smach/io/local_data_accessor.py
+++ vehicle_diag_smach/io/local_data_accessor.py
@@ -4,12 +4,13 @@
 
 import os
 import shutil
+from pathlib import Path
 from typing import List
 
 from oscillogram_classification import preprocess
 
-from vehicle_diag_smach.config import DUMMY_ISOLATION_OSCILLOGRAM_NEG1, SESSION_DIR, OSCI_ISOLATION_SESSION_FILES, \
-    DUMMY_ISOLATION_OSCILLOGRAM_NEG2, DUMMY_ISOLATION_OSCILLOGRAM_POS
+from vehicle_diag_smach.config import DUMMY_OSCILLOGRAMS, SESSION_DIR, OSCI_SESSION_FILES, \
+    DUMMY_ISOLATION_OSCILLOGRAM_POS, DUMMY_ISOLATION_OSCILLOGRAM_NEG1, DUMMY_ISOLATION_OSCILLOGRAM_NEG2
 from vehicle_diag_smach.data_types.onboard_diagnosis_data import OnboardDiagnosisData
 from vehicle_diag_smach.data_types.oscillogram_data import OscillogramData
 from vehicle_diag_smach.data_types.workshop_data import WorkshopData
@@ -45,6 +46,29 @@ class LocalDataAccessor(DataAccessor):
         print(obd_data)
         return obd_data
 
+    @staticmethod
+    def create_local_dummy_oscillograms():
+        """
+        Creates local dummy oscillograms to be used in the classification states.
+        """
+        # set up directory for osci classification
+        osci_session_dir = SESSION_DIR + "/" + OSCI_SESSION_FILES + "/"
+        if not os.path.exists(osci_session_dir):
+            os.makedirs(osci_session_dir)
+
+        # create dummy oscillograms in '/session_files'
+        for path in Path(DUMMY_OSCILLOGRAMS).rglob('*.csv'):
+            src = str(path)
+            shutil.copy(src, osci_session_dir + str(src.split("/")[-1]))
+
+        # create dummy oscillograms for fault isolation
+        # TODO: hard-coded for demo purposes - showing reasonable case
+        shutil.copy(DUMMY_ISOLATION_OSCILLOGRAM_NEG1, osci_session_dir + "Ladedruck-Regelventil" + ".csv")
+        shutil.copy(DUMMY_ISOLATION_OSCILLOGRAM_NEG2, osci_session_dir + "Ladedrucksteller-Positionssensor" + ".csv")
+        shutil.copy(DUMMY_ISOLATION_OSCILLOGRAM_POS, osci_session_dir + "VTG-Abgasturbolader" + ".csv")
+        shutil.copy(DUMMY_ISOLATION_OSCILLOGRAM_POS, osci_session_dir + "Motor-Steuergerät" + ".csv")
+        shutil.copy(DUMMY_ISOLATION_OSCILLOGRAM_NEG1, osci_session_dir + "Ladedruck-Magnetventil" + ".csv")
+
     def get_oscillograms_by_components(self, components: List[str]) -> List[OscillogramData]:
         """
         Retrieves the oscillogram data for the specified components.
@@ -52,23 +76,10 @@ class LocalDataAccessor(DataAccessor):
         :param components: components to retrieve oscillograms for
         :return: oscillogram data for each component
         """
+        self.create_local_dummy_oscillograms()
         oscillograms = []
-
-        # set up directory if not present
-        osci_iso_session_dir = SESSION_DIR + "/" + OSCI_ISOLATION_SESSION_FILES + "/"
-        if not os.path.exists(osci_iso_session_dir):
-            os.makedirs(osci_iso_session_dir)
-
         for comp in components:
-            # TODO: hard-coded for demo purposes - showing reasonable case (one NEG)
-            if comp == "Ladedruck-Regelventil":
-                shutil.copy(DUMMY_ISOLATION_OSCILLOGRAM_NEG1, osci_iso_session_dir + comp + ".csv")
-            elif comp == "Ladedrucksteller-Positionssensor":
-                shutil.copy(DUMMY_ISOLATION_OSCILLOGRAM_NEG2, osci_iso_session_dir + comp + ".csv")
-            else:
-                shutil.copy(DUMMY_ISOLATION_OSCILLOGRAM_POS, osci_iso_session_dir + comp + ".csv")
-            path = SESSION_DIR + "/" + OSCI_ISOLATION_SESSION_FILES + "/" + comp + ".csv"
+            path = SESSION_DIR + "/" + OSCI_SESSION_FILES + "/" + comp + ".csv"
             _, voltages = preprocess.read_oscilloscope_recording(path)
             oscillograms.append(OscillogramData(voltages, comp))
-
         return oscillograms
