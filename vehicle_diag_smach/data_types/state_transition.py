fb608c14946c1084ac500acd19a03884eceb4499
tbohne <tim.bohne@dfki.de>
2023-06-21 11:45:34 +0200

    impl data type for state transitions

---
+++ vehicle_diag_smach/data_types/state_transition.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @author Tim Bohne
+
+class StateTransition:
+    """
+    Represents a transition in the state machine from one state to another using a particular link.
+    """
+
+    def __init__(self, prev_state: str, curr_state: str, transition_link: str):
+        """
+        Inits the state transition.
+
+        :param prev_state: previous state
+        :param curr_state: current state
+        :param transition_link: used transition
+        """
+        self.prev_state = prev_state
+        self.curr_state = curr_state
+        self.transition_link = transition_link
+
+    def __str__(self):
+        return self.prev_state + " --- (" + self.transition_link + ") ---> " + self.curr_state
