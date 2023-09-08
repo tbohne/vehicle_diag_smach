431e62553765d6d5dcc20cf2cda9bd7753162bd9
tbohne <tim.bohne@dfki.de>
2023-09-08 12:17:23 +0200

    refactor / refine ESTABLISH_INITIAL_HYPOTHESIS

--- vehicle_diag_smach/high_level_states/establish_initial_hypothesis.py
+++ vehicle_diag_smach/high_level_states/establish_initial_hypothesis.py
@@ -10,8 +10,7 @@ from bs4 import BeautifulSoup
 from obd_ontology import ontology_instance_generator, knowledge_graph_query_tool
 from termcolor import colored
 
-from vehicle_diag_smach.config import SESSION_DIR, XPS_SESSION_FILE, HISTORICAL_INFO_FILE, CC_TMP_FILE, \
-    OBD_INFO_FILE
+from vehicle_diag_smach.config import SESSION_DIR, XPS_SESSION_FILE, HISTORICAL_INFO_FILE, CC_TMP_FILE, OBD_INFO_FILE
 from vehicle_diag_smach.data_types.state_transition import StateTransition
 from vehicle_diag_smach.interfaces.data_provider import DataProvider
 
@@ -38,11 +37,48 @@ class EstablishInitialHypothesis(smach.State):
         self.qt = knowledge_graph_query_tool.KnowledgeGraphQueryTool(kg_url=kg_url)
 
     @staticmethod
-    def log_state_info():
+    def log_state_info() -> None:
+        """
+        Logs the state information.
+        """
         os.system('cls' if os.name == 'nt' else 'clear')
         print("\n\n############################################")
         print("executing", colored("ESTABLISH_INITIAL_HYPOTHESIS", "yellow", "on_grey", ["bold"]), "state..")
         print("############################################")
+
+    @staticmethod
+    def read_initial_hypothesis() -> str:
+        """
+        Reads the initial hypothesis from the session directory (customer complaints).
+
+        :return: initial hypothesis based on customer complaints
+        """
+        try:
+            with open(SESSION_DIR + "/" + XPS_SESSION_FILE) as f:
+                data = f.read()
+                session_data = BeautifulSoup(data, 'xml')
+                for tag in session_data.find_all('rating', {'type': 'heuristic'}):
+                    return tag.parent['objectName']
+        except FileNotFoundError:
+            print("no customer complaints available..")
+            return ""
+
+    def handle_insufficient_data(self) -> None:
+        """
+        Handles 'insufficient data' cases, i.e., cases in which no OBD data and no customer complaints are available.
+        """
+        self.data_provider.provide_state_transition(StateTransition(
+            "ESTABLISH_INITIAL_HYPOTHESIS", "insufficient_data", "no_DTC_and_no_CC"
+        ))
+        with open(SESSION_DIR + '/metadata.json', 'r') as f:  # read meta data
+            data = json.load(f)
+        with open(SESSION_DIR + "/" + OBD_INFO_FILE, "r") as f:  # read OBD data
+            obd_data = json.load(f)
+        vehicle_id = self.qt.query_vehicle_instance_by_vin(obd_data["vin"])[0].split("#")[1]
+        # extend KG with `DiagLog` instance
+        self.instance_gen.extend_knowledge_graph_with_diag_log(
+            data["diag_date"], data["max_num_of_parallel_rec"], obd_data["dtc_list"], [], [], vehicle_id
+        )
 
     def execute(self, userdata: smach.user_data.Remapper) -> str:
         """
@@ -52,53 +88,25 @@ class EstablishInitialHypothesis(smach.State):
         :return: outcome of the state ("established_init_hypothesis" | "no_DTC_and_no_CC")
         """
         self.log_state_info()
-
         print("\nreading customer complaints session protocol..")
-        initial_hypothesis = ""
-        try:
-            with open(SESSION_DIR + "/" + XPS_SESSION_FILE) as f:
-                data = f.read()
-                session_data = BeautifulSoup(data, 'xml')
-                for tag in session_data.find_all('rating', {'type': 'heuristic'}):
-                    initial_hypothesis = tag.parent['objectName']
-        except FileNotFoundError:
-            print("no customer complaints available..")
+        initial_hypothesis = self.read_initial_hypothesis()
 
         if len(userdata.vehicle_specific_instance_data.dtc_list) == 0 and len(initial_hypothesis) == 0:
-            # no OBD data + no customer complaints -> insufficient data
-            self.data_provider.provide_state_transition(StateTransition(
-                "ESTABLISH_INITIAL_HYPOTHESIS", "insufficient_data", "no_DTC_and_no_CC"
-            ))
-            # read meta data
-            with open(SESSION_DIR + '/metadata.json', 'r') as f:
-                data = json.load(f)
-            # read OBD data
-            with open(SESSION_DIR + "/" + OBD_INFO_FILE, "r") as f:
-                obd_data = json.load(f)
-
-            # read vehicle ID
-            vehicle_id = self.qt.query_vehicle_instance_by_vin(obd_data["vin"])[0].split("#")[1]
-
-            # extend KG with `DiagLog` instance
-            self.instance_gen.extend_knowledge_graph_with_diag_log(
-                data["diag_date"], data["max_num_of_parallel_rec"], obd_data["dtc_list"], [], [], vehicle_id
-            )
+            self.handle_insufficient_data()  # no OBD data + no customer complaints -> insufficient data
             return "no_DTC_and_no_CC"
 
         print("reading historical information..")
         with open(SESSION_DIR + "/" + HISTORICAL_INFO_FILE) as f:
-            data = f.read()
+            data = f.read()  # TODO: we don't do anything with it yet
 
         if len(initial_hypothesis) > 0:
-            print("initial hypothesis based on customer complaints available..")
-            print("initial hypothesis:", initial_hypothesis)
+            print("initial hypothesis based on customer complaints available:", initial_hypothesis)
             userdata.hypothesis = initial_hypothesis
             unused_cc = {'list': [initial_hypothesis]}
             with open(SESSION_DIR + "/" + CC_TMP_FILE, 'w') as f:
                 json.dump(unused_cc, f, default=str)
         else:
             print("no initial hypothesis based on customer complaints..")
-
         # TODO: use historical data to refine initial hypothesis (e.g. to deny certain hypotheses)
         print("establish hypothesis..")
         self.data_provider.provide_state_transition(StateTransition(
