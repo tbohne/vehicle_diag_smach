1e94c259d15e585930d3f6041d4cab03fd72d8fd
tbohne <tim.bohne@dfki.de>
2023-09-27 15:35:06 +0200

    implement KG checks for test scenario two

--- vehicle_diag_smach/tests/test_high_level_smach.py
+++ vehicle_diag_smach/tests/test_high_level_smach.py
@@ -111,6 +111,41 @@ class TestHighLevelStateMachine(unittest.TestCase):
             sorted(['C20 -> C19 -> C18 -> C15 -> C14', 'C21 -> C19 -> C18 -> C15 -> C14'])
         )
 
+    def test_model_availability_for_scenario_two(self) -> None:
+        """
+        Tests the availability of expected trained models for test scenario zero.
+        """
+        for i in range(23, 36, 2):  # all odd components are expected to have a trained model
+            self.assertTrue(os.path.exists("res/trained_model_pool/C" + str(i) + ".h5"))
+
+    def test_hosted_knowledge_graph_for_scenario_two(self) -> None:
+        """
+        Tests whether the correct (expected) KG is hosted for the unit tests:
+        https://github.com/tbohne/obd_ontology/tree/main/knowledge_base/unit_test_kg.nt.
+        Essentially, it verifies that the KG contains the assumed knowledge for test scenario two.
+        """
+        qt = knowledge_graph_query_tool.KnowledgeGraphQueryTool(kg_url=KG_URL)
+
+        for i in range(22, 37):  # all expected components have to exist in the KG
+            self.assertEqual(len(qt.query_suspect_component_by_name("C" + str(i))), 1)
+
+        # check expected `affected_by` relations
+        self.assertListEqual(sorted(qt.query_affected_by_relations_by_suspect_component("C22")), sorted(["C23", "C24"]))
+        self.assertListEqual(sorted(qt.query_affected_by_relations_by_suspect_component("C24")), sorted(["C25", "C26"]))
+        self.assertListEqual(sorted(qt.query_affected_by_relations_by_suspect_component("C25")), ["C27"])
+        self.assertListEqual(sorted(qt.query_affected_by_relations_by_suspect_component("C26")), ["C28"])
+        self.assertListEqual(sorted(qt.query_affected_by_relations_by_suspect_component("C27")), ["C29"])
+        self.assertListEqual(sorted(qt.query_affected_by_relations_by_suspect_component("C28")), ["C29"])
+        self.assertListEqual(sorted(qt.query_affected_by_relations_by_suspect_component("C30")), ["C31"])
+        self.assertListEqual(sorted(qt.query_affected_by_relations_by_suspect_component("C31")), sorted(["C32", "C33"]))
+        self.assertListEqual(sorted(qt.query_affected_by_relations_by_suspect_component("C32")), ["C34"])
+        self.assertListEqual(sorted(qt.query_affected_by_relations_by_suspect_component("C33")), ["C34"])
+        self.assertListEqual(sorted(qt.query_affected_by_relations_by_suspect_component("C34")), sorted(["C35", "C36"]))
+
+        # expected entry components
+        self.assertListEqual(qt.query_suspect_components_by_dtc("P0127"), ["C22"])
+        self.assertListEqual(qt.query_suspect_components_by_dtc("P0128"), ["C30"])
+
 
 if __name__ == '__main__':
     smach.set_loggers(log_info, log_debug, log_warn, log_err)  # set custom logging functions
