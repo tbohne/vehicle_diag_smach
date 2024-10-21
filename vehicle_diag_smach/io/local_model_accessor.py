ca21a16cbfe2d0ae1b5fffab2da0118051321b5a
tbohne <tim.bohne@dfki.de>
2024-10-21 14:25:17 +0200

    impl local model accessor for rule-based models

--- vehicle_diag_smach/io/local_model_accessor.py
+++ vehicle_diag_smach/io/local_model_accessor.py
@@ -9,6 +9,9 @@ from tensorflow import keras
 
 from vehicle_diag_smach.config import TRAINED_MODEL_POOL, FINAL_DEMO_MODELS
 from vehicle_diag_smach.interfaces.model_accessor import ModelAccessor
+from vehicle_diag_smach.interfaces.rule_based_model import RuleBasedModel
+from vehicle_diag_smach.rule_based_models.Lambdasonde import Lambdasonde
+from vehicle_diag_smach.rule_based_models.Saugrohrdrucksensor import Saugrohrdrucksensor
 
 
 class LocalModelAccessor(ModelAccessor):
@@ -46,6 +49,30 @@ class LocalModelAccessor(ModelAccessor):
         except OSError as e:
             print("no trained model available for the signal (component) to be classified:", component)
             print("ERROR:", e)
+
+    def get_rule_based_univariate_ts_classification_model_by_component(
+            self, component: str
+    ) -> Union[Tuple[RuleBasedModel, dict], None]:
+        """
+        Retrieves a rule-based model to classify signals of the specified vehicle component.
+
+        The provided model is expected to be a rule-based model satisfying the following assumptions:
+            - input_shape: (len_of_ts, 1)
+            - output_shape: bool
+
+        :param component: vehicle component to retrieve rule-based model for
+        :return: rule-based model and model meta info dictionary or `None` if unavailable
+        """
+        # TODO: I could obtain these information from the KG
+        model_meta_info = {
+            "normalization_method": "z_norm",
+            "model_id": "rule_based_univariate_ts_classification_model_001"
+        }
+        if "Lambdasonde" in component:
+            return Lambdasonde(), model_meta_info
+        elif "Saugrohrdrucksensor" in component:
+            return Saugrohrdrucksensor(), model_meta_info
+        return None
 
     def get_torch_multivariate_ts_classification_model_by_component(
             self, component: str
