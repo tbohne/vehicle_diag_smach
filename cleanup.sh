460d69356d39175c3c0676be02a9443c25acd636
tbohne <tim.bohne@dfki.de>
2024-11-14 17:58:54 +0100

    extend cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -4,3 +4,8 @@ pip uninstall obd_ontology
 pip uninstall oscillogram_classification
 pip uninstall dtc_parser
 pip uninstall vehicle_diag_smach
+pip uninstall tsai
+pip uninstall torch
+pip uninstall torchvision
+pip uninstall sktime
+pip cache purge
